7db2437f4dfd7b89eb33e091ec3f618158344403
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2023-06-18 14:04:08 +0530

    Added a README file and also incorporated a test_project python file checking the custom functions.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Stock Market
+
+#### Video Demo: "link"
+
+#### Description: A basic stock market data science app that displays stock price of GOOGLE and MICROSFT.
+
+#### It uses the 'streamlit' library to deploy the web application and tabulate and graphically represent the data.
+
+#### It uses the 'yfinance' library to take in the information and stock price data of the aforementioned companies.
--- project.py
+++ project.py
@@ -7,7 +7,15 @@ def get_ticker(name):
     return finance.Ticker(name)
 
 
-def market():
+def download(tag, start, end):
+    return finance.download(tag, start=start, end=end)
+
+
+def history(comp, time):
+    return comp.history(period=time)
+
+
+def main():
 
     st.title("Stock Market App")
     st.header("Data Science Application")
@@ -16,11 +24,11 @@ def market():
     comp1 = get_ticker("GOOGL")
     comp2 = get_ticker("MSFT")
 
-    google = finance.download("GOOGL", start="2023-05-01", end="2023-05-01")
-    microsoft = finance.download("MSFT", start="2023-05-01", end="2023-05-01")
+    google = download("GOOGL", "2023-05-01", "2023-05-01")
+    microsoft = download("MSFT", "2023-05-01", "2023-05-01")
 
-    data1 = comp1.history(period="3mo")
-    data2 = comp2.history(period="3mo")
+    data1 = history(comp1, "3mo")
+    data2 = history(comp2, "3mo")
 
     st.write("""
     ### Google
@@ -42,4 +50,4 @@ def market():
 
 
 if __name__ == '__main__':
-    market()
+    main()
